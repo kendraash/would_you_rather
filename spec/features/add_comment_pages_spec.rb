ac6d5396d3afe47aac33a2631d1b1be552fb2c51
Kendra Ash & Alyssa Horrocks <student@epicodus.com>
2015-10-26 14:22:17 -0700

    Add 100 percent coverage for testing

--- spec/features/add_comment_pages_spec.rb
+++ spec/features/add_comment_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'add a comment' do
-  it 'allow user to add a new comment to a choice using AJAX', remote: true do
+  it 'allow user to add a new comment to a choice using AJAX', js: true do
     choice = Choice.create({option_a: "Pie", option_b: "Cake"})
     visit root_path
     click_on "Add Comment"
@@ -10,4 +10,14 @@ describe 'add a comment' do
     click_on "Create Comment"
     expect(page).to have_content("Always Pie")
   end
+
+  it "gives error when no content is entered" do
+    choice = Choice.create({option_a: "Pie", option_b: "Cake"})
+    visit root_path
+    click_on "Add Comment"
+    fill_in 'Comment', :with => ""
+    click_on "Create Comment"
+    expect(page).to have_content 'errors'
+  end
+
 end
