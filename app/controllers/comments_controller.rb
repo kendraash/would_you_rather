2d63acc38433435c23aa5be27d24694bb06421d9
Kendra Ash & Alyssa Horrocks <student@epicodus.com>
2015-10-26 11:49:01 -0700

    Add a comment button on the homepage for each choice

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+
+  def new
+    @choice = Choice.find(params[:choice_id])
+    @comment = @choice.comments.new
+  end
+
+  def create
+    @choice = Choice.find(params[:choice_id])
+    @comment = @choice.comments.new(comment_params)
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
-  resources :choices, :except => [:index]
+  resources :choices, :except => [:index, :show, :edit] do
+    resources :comments
+  end
 end
